ba995628ef2048eb80338bef5793f8fed74d1783
Douglas Gonzales <gonzales_douglas@outlook.com>
2017-06-13 10:02:43 -0500

    añadido de geolocalizacion

--- app/src/main/java/com/dgonzales/douglas/clue/Dashboard.java
+++ app/src/main/java/com/dgonzales/douglas/clue/Dashboard.java
@@ -98,18 +98,17 @@ public class Dashboard extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.scanner) {
+            Intent intent =  new Intent(Dashboard.this,camera.class);
+            startActivity(intent);
+        } else if (id == R.id.lista) {
+            //handle lista actions
+        } else if (id == R.id.ubicacion) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+            Intent intent = new Intent(Dashboard.this,MapsActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.herramientas) {
+            //handle messagge
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
